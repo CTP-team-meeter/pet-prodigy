60f3f669868cee5e6fe0ad3da9c9e10a054ddc97
shin <sshoony95@gmail.com>
2023-02-07 18:02:21 -0500

    Remove useless files

--- server/controllers/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express = require('express');
-var router = express.Router();
-// Import all the routes
-var catBreedsController = require('./catBreeds');
-// Use the routes
-router.use('/catBreeds', catBreedsController);
-module.exports = router;
