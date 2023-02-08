5febab914620791795c13e158e155f12c018b9af
shin <sshoony95@gmail.com>
2023-02-08 18:25:10 -0500

    Add a user model

---
+++ server/models/user.ts
@@ -0,0 +1,11 @@
+// Avoids typescript from making global modules
+export {};
+
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  user_name: { type: String, required: true, unique: true },
+  password: { type: String, required: true}
+});
+
+module.exports = mongoose.model('User', userSchema);
