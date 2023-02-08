aaf7c2125286fe15b1a453c8268ace5e21849a11
shin <sshoony95@gmail.com>
2023-02-08 11:16:58 -0500

    Refactore seeding function into a module

--- server/index.ts
+++ server/index.ts
@@ -11,6 +11,7 @@ const app = express();
 
 // Importing mongoose
 const mongoose = require('mongoose');
+const { seedPet } = require('./utils/petSeeder');
 
 // Enable CORS
 const cors = require('cors');
@@ -35,44 +36,9 @@ db.once('open', () => {
   console.log('Connected to MongoDB');
 });
 
-/* Should be refactored into a separate module */
-const seedCatbreeds = async () => {
-  const CatBreed = require('./models/catBreed');
-  const res = await fetch('https://api.thecatapi.com/v1/breeds');
-  const data = await res.json();
-  data.forEach(async (cat: any) => {
-    const imgRes = await fetch(`https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=${cat.id}`);
-    const images = await imgRes.json()
-    const newCat =  new CatBreed({
-      name: cat.name,
-      imageURLs: images,
-      origin: cat.origin,
-      life_span: cat.life_span,
-      temperament: cat.temperament,
-      wikipedia_url: cat.wikipedia_url,
-      weight: cat.weight,
-      height: cat.height,
-      adaptability: cat.adaptability,
-      affection_level: cat.affection_level,
-      child_friendly: cat.child_friendly,
-      grooming: cat.grooming,
-      intelligence: cat.intelligence,
-      health_issues: cat.health_issues,
-      social_needs: cat.social_needs,
-      stranger_friendly: cat.stranger_friendly,
-      vocalisation: cat.vocalisation,
-      energy_level: cat.energy_level,
-      description: cat.description
-    });
-    console.log(images);
-    newCat.save((err: any,res: any)=>{
-      console.log(err);
-      console.log(res);
-    });  
-  });
-};
-seedCatbreeds();
-/*  */
+
+seedPet(require('./models/catBreed'));
+
 
 app.use(express.json());
 
