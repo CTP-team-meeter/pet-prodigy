ceebe5330e48f059ab49e1f6a3662c00725c69d2
shin <sshoony95@gmail.com>
2023-02-09 10:58:12 -0500

    Add a feature to hash password and authenticate with it

--- server/controllers/auth.ts
+++ server/controllers/auth.ts
@@ -3,20 +3,25 @@ export {};
 
 const User = require("../models/user");
 const { generateAccessToken } = require("../middlewares/authentication");
+const bcrypt = require("bcrypt");
 
 exports.signup = async (req: any, res: any) => {
   try {
+    const hashedPassword = bcrypt.hashSync(
+      req.body.password,
+      Number(process.env.SALT)
+    );
     console.log("POST body: ", req.body);
     const newUser = await new User({
       username: req.body.username,
-      password: req.body.password,
+      password: hashedPassword,
     });
     await newUser.save();
 
     const token = await generateAccessToken({ username: req.body.username });
     res.status(200).json(token);
   } catch (err) {
-    if (User.exists({ username: req.body.username })) {
+    if (await User.exists({ username: req.body.username })) {
       res.status(400).json({
         msg: "This username is already taken. Enter a different username",
         err,
@@ -34,14 +39,17 @@ exports.login = async (req: any, res: any) => {
         msg: "This username doesn't exist. Enter a different username",
       });
     }
-    const user = await User.find({
+    const user = await User.findOne({
       username: req.body.username,
-      password: req.body.password,
     });
-
-    const token = await generateAccessToken({ username: req.body.username });
-
-    res.status(200).json(token);
+    if (bcrypt.compareSync(req.body.password, user.password)) {
+      const token = await generateAccessToken({ username: req.body.username });
+      res.status(200).json(token);
+    } else {
+      res
+        .status(400)
+        .json({ msg: "Invalid password, Enter a correct password." });
+    }
   } catch (err) {
     res.status(400).json({ msg: "Failed Login", err });
   }
