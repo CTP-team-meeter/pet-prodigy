cabf0e74ecde1b8ab0bf0e6b8639bd7008d1c5e7
shin <sshoony95@gmail.com>
2023-03-01 10:09:15 -0500

    Fix color mode :

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,6 +6,7 @@ import Landing from './pages/Landing';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import useLocalStorage from "use-local-storage";
+import { useEffect } from 'react';
 
 function App() {
   const defaultDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
@@ -16,8 +17,11 @@ function App() {
   const themeToggle = () => {
     setTheme(theme == "light" ? "dark" : "light");
   };
+  useEffect(()=>{
+    document.body.setAttribute("data-theme", theme);
+  },[theme])
   return (
-    <div className="App" data-theme={theme}>
+    <div className="App bg-primary text-primary">
       <Router>
         <Navbar themeToggle={themeToggle} />
         <Routes>
