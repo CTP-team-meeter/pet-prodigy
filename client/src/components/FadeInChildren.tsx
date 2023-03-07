85ef7c02bc3375be08aeec817362a6e9bc371375
shin <sshoony95@gmail.com>
2023-03-07 09:13:19 -0500

    Add note

--- client/src/components/FadeInChildren.tsx
+++ client/src/components/FadeInChildren.tsx
@@ -1,5 +1,16 @@
 import { useState, useRef, useEffect, Children, cloneElement } from "react";
 import "./FadeInChildren.css";
+/*Note
+  The FadeInChildren component is a custom React component 
+  that allows for a fade-in animation effect to be applied to its child components.
+  
+  The component takes in two props:
+    1. children: This prop is required and represents 
+     the child components that are to be animated.
+    2. toLeft: This prop is optional and represents the direction of the animation.
+     If set to true, the animation will appear from the right side. 
+     Otherwise, it will appear from the left side.
+ */
 const FadeInChildren = ({
   children,
   toLeft,
