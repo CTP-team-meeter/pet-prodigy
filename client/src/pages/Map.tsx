40bed8ea2bfb1d9b77ecbd1a3c6f8be75a7c2de7
Kevin Docanto <kevin-teus@hotmail.com>
2023-03-11 17:48:00 -0500

    Map location added.

--- client/src/pages/Map.tsx
+++ client/src/pages/Map.tsx
@@ -1,31 +1,39 @@
 import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
-import { memo, useCallback, useState } from 'react';
+import { memo, useCallback, useEffect, useState } from 'react';
+import { TailSpin } from 'react-loader-spinner';
 
 const containerStyle = {
-  width: '400px',
-  height: '400px',
+  width: '1024px',
+  height: '600px',
 };
 
-const center = {
-  lat: -3.745,
-  lng: -38.523,
-};
+function Map() {
+  const [position, setPosition] = useState(null);
+  const [map, setMap] = useState(null);
 
-function MyComponent() {
   const { isLoaded } = useJsApiLoader({
     id: 'google-map-script',
     googleMapsApiKey: import.meta.env.VITE_GOOGLE_MAPS_API_KEY,
   });
 
-  const [map, setMap] = useState(null);
-
-  const onLoad = useCallback(function callback(map: any) {
-    // This is just an example of getting and using the map instance!!! don't just blindly copy!
-    const bounds = new window.google.maps.LatLngBounds(center);
-    map.fitBounds(bounds);
-
-    setMap(map);
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position: any) => {
+      setPosition(position.coords);
+    });
   }, []);
+
+  const onLoad = useCallback(
+    (map: any) => {
+      if (position) {
+        const bounds = new window.google.maps.LatLngBounds(
+          new window.google.maps.LatLng(position.latitude, position.longitude)
+        );
+        map.fitBounds(bounds);
+      }
+      setMap(map);
+    },
+    [position]
+  );
 
   const onUnmount = useCallback(function callback(map: any) {
     setMap(null);
@@ -35,15 +43,17 @@ function MyComponent() {
     <div className="flex justify-center">
       <GoogleMap
         mapContainerStyle={containerStyle}
-        center={center}
-        zoom={10}
+        center={{ lat: position?.latitude, lng: position?.longitude }}
         onLoad={onLoad}
         onUnmount={onUnmount}
+        zoom={18}
       ></GoogleMap>
     </div>
   ) : (
-    <></>
+    <div className="flex justify-center items-center mt-32 h-96">
+      <TailSpin width={100} height={100} color="#00BFFF" />
+    </div>
   );
 }
 
-export default memo(MyComponent);
+export default memo(Map);
