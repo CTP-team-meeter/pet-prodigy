be9afe484b6cfa350a9c20bfb191238054bb0944
Kevin Docanto <kevin-teus@hotmail.com>
2023-07-03 18:00:24 -0400

    Docker changes.

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,19 +2,22 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default ({mode}) => {
-  process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
   return defineConfig({
     plugins: [react()],
     server: {
       host: true,
       proxy: {
         '/api': {
-          target: 'http://localhost:8080',
+          target: 'http://0.0.0.0:8080',
           changeOrigin: true,
           secure: false,
         },
       },
     },
-  })
+    build: {
+      target: 'es2020', // or 'es2022' if you prefer
+    },
+  });
 };
